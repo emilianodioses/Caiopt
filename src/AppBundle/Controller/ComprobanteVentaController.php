6dd57d89f9cfdfbaf353a78fe2f2c14ec6f0a0cb
Ivan Rizzo <ivanrizzo@gmail.com>
2019-01-05 10:53:59 -0300

    afip xml git ignore

--- src/AppBundle/Controller/ComprobanteVentaController.php
+++ src/AppBundle/Controller/ComprobanteVentaController.php
@@ -272,8 +272,9 @@ class ComprobanteVentaController extends Controller
         $afip = $this->get('AfipFE');
 
         //Lo siguiente debería modificarse para que no quede hardcodeado
-        //dump($afip->getWS()->ElectronicBilling->GetVoucherTypes());
-        dump($afip->getWS()->ElectronicBilling->GetAliquotTypes());
+        dump($afip->getWS()->ElectronicBilling->GetVoucherTypes());
+        //dump($afip->getWS()->ElectronicBilling->GetAliquotTypes());
+        //var_export($afip->getWS()->ElectronicBilling->GetAliquotTypes());
         die;
         switch ($comprobante->getTipo()) {
             case 'Factura A':
