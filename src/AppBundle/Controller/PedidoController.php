815df61200d4884056f6d426f133a0108d683c60
fracich <fernandoracich@gmail.com>
2019-11-29 14:08:50 -0300

    issue#30

--- src/AppBundle/Controller/PedidoController.php
+++ src/AppBundle/Controller/PedidoController.php
@@ -283,7 +283,7 @@ class PedidoController extends controller
         
         $em->flush();
         
-        return $this->redirectToRoute('pedidoventa_index');
+        return $this->redirectToRoute('pedido_index');
     }
 
     /**
@@ -300,5 +300,51 @@ class PedidoController extends controller
             ->setMethod('DELETE')
             ->getForm()
         ;
+    }
+
+    /**
+     * Imprime la factura electrónica generada por medio de WS con afip.
+     *
+     */
+    public function imprimirAction(Request $request, Pedido $pedido)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $pedidoDetalles = $em->getRepository('AppBundle:PedidoDetalle')->findBy(Array('pedido'=>$pedido,  'activo'=>1));
+
+        $facturaTemplate = 'pedido/imprimir.html.twig';
+    
+        $html = $this->renderView($facturaTemplate, array(
+            'pedido' => $pedido,
+            'pedidoDetalles' => $pedidoDetalles,
+            'empresa' => $this->container->getParameter('empresa'),
+            'empresaRazonSocial' => $this->container->getParameter('empresa_razon_social'),
+            'empresaCondicion' => $this->container->getParameter('empresa_condicion'),
+            'empresaCuit' => $this->container->getParameter('empresa_cuit'),
+            'empresaIngresosBrutos' => $this->container->getParameter('empresa_ingresos_brutos'),
+            'empresaInicioActividades' => $this->container->getParameter('empresa_inicio_actividades'),
+        )
+        );
+
+        //set_time_limit(30); uncomment this line according to your needs
+        // If you are not in a controller, retrieve of some way the service container and then retrieve it
+        //$pdf = $this->container->get("white_october.tcpdf")->create('vertical', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        //if you are in a controlller use :
+        $pdf = $this->get("white_october.tcpdf")->create('vertical', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        // remove default header/footer
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+        //$pdf->SetAuthor('Our Code World');
+        //$pdf->SetTitle(('Our Code World Title'));
+        //$pdf->SetSubject('Our Code World Subject');
+        //$pdf->setFontSubsetting(true);
+        $pdf->SetFont('helvetica', '', 11, '', true);
+        //$pdf->SetMargins(20,20,40, true);
+        $pdf->AddPage();
+        
+        $filename = 'Pedido_'.$pedido->getNumero();
+        
+        $pdf->writeHTMLCell($w = 0, $h = 0, $x = '', $y = '', $html, $border = 0, $ln = 1, $fill = 0, $reseth = true, $align = '', $autopadding = true);
+        $pdf->Output($filename.".pdf",'I'); // This will output the PDF as a response directly
     }
 }
