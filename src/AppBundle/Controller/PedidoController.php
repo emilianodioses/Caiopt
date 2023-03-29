810c64e165e847306233121a3c4382dd2d6f8dbb
jesimedina <ye.medina86@gmail.com>
2023-03-29 19:21:59 -0300

    Elimino campos

--- src/AppBundle/Controller/PedidoController.php
+++ src/AppBundle/Controller/PedidoController.php
@@ -100,10 +100,7 @@ class PedidoController extends controller
                     $articulo->setMarca($marca);
                     $articulo->setDescripcion($pedidoDetalle->getArticulo()->getDescripcion());
                     $articulo->setIva($iva_21); //Asigno 21% de iva, después habría si es correcto
-//                    $articulo->setGenero('');
-                    $articulo->setMaterial('');
                     $articulo->setForma('');
-//                    $articulo->setEstilo('');
                     $articulo->setTipoAro('');
                     $articulo->setColorMarco('');
                     $articulo->setColorCristal('');
