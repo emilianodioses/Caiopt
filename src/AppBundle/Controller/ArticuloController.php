5e098027acdedcf5e8b9ce4a81f7d3647ffe000d
Manuel-Ferraretto <manuferraretto86@gmail.com>
2023-10-19 08:46:21 -0300

    fix conflicto

--- src/AppBundle/Controller/ArticuloController.php
+++ src/AppBundle/Controller/ArticuloController.php
@@ -291,7 +291,7 @@ class ArticuloController extends Controller
     {
         // Permisos de Usuario para Acciones
         $secure = $this->container->get('SecureAction');
-        
+
         if (!$secure->isAuthorized('ArticuloCategoria', 'Index', $this->getUser()->getRol())):
             return new Response('Acceso denegado. Por favor solicite acceso al administrador de sistema.');
         endif;
@@ -301,10 +301,12 @@ class ArticuloController extends Controller
         $categorias = $em->getRepository('AppBundle:ArticuloCategoria')->findBy(array('activo' => true));
         $marca = $request->query->get('marca', 0);
         $categoria = $request->query->get('categoria', 0);
+        $updated_by = $this->getUser()->getId();
+        $updated_at = new \DateTime("now");
 
         if (isset($_GET['ajustec'])) {
             $ajuste = $request->query->get('ajuste', 0);
-            $em->getRepository('AppBundle:Articulo')->updateByAjuste($marca,$categoria,$ajuste);
+            $em->getRepository('AppBundle:Articulo')->updateByAjuste($marca, $categoria, $ajuste, $updated_by, $updated_at);
         }
         $articulos = $em->getRepository('AppBundle:Articulo')->findByAjuste($marca,$categoria);
 
